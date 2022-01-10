15989e5834b2862c4e4c97b2a2c4fc644cac0456
Alexander Staeding <alexanderstaeding@gmail.com>
2022-01-10 18:51:54 +0100

    2 -> 4 spaces

--- src/main/java/h07/Main.java
+++ src/main/java/h07/Main.java
@@ -1,12 +1,12 @@
 package h07;
 
 public class Main {
-  /**
-   * Main entry point in executing the program
-   *
-   * @param args program arguments, currently ignored
-   */
-  public static void main(String[] args) {
-    System.out.println("Eine beispielhafte Main-Methode, die Sie z.B. zum Ausprobieren nutzen können. Diese wird nicht bewertet.");
-  }
+    /**
+     * Main entry point in executing the program
+     *
+     * @param args program arguments, currently ignored
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
